cb7c5655934086ab0054cc728a5845c10587ffe3
lemnisSP <35028260+lemnisSP@users.noreply.github.com>
2018-03-21 14:07:50 +0100

    Added stylelint

---
+++ stylelint-config-studyportals/index.md
@@ -0,0 +1,36 @@
+
+# Variables
+
+All variables must start with an capital and exist out of letters
+
+```scss
+$GoodExample: true;
+
+$badExample: false;
+$_AlsoAnBadExample: false;
+$still-a-bad-example: false;
+```
+
+# Functions
+
+Function names can have letters or `-`
+
+```scss
+@function good {}
+@function Example {}
+@function good-example {}
+
+@function another_bad_example {}
+```
+
+# Mixins
+
+Mixin name can only have letters
+
+```scss
+@mixin good {}
+@mixin Example {}
+
+@mixin bad-example {}
+@mixin another_bad_example {}
+```
---
+++ stylelint-config-studyportals/stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "processor": ["stylelint-processor-arbitrary-tags"],
+    "plugins": ["stylelint-scss"],
+    "rules": {
+        "scss/dollar-variable-pattern": "^[A-Z][a-zA-Z]*$",
+        "scss/at-mixin-pattern": "^[a-zA-Z]+$",
+        "scss/percent-placeholder-pattern": "^[A-Z][a-zA-Z]*$",
+        "scss/at-function-pattern": "^[a-zA-Z]*-?[a-zA-Z]*$",
+        "selector-class-pattern": "^[a-zA-Z]*$",
+        "selector-id-pattern": "^[a-zA-Z]+$",
+        "max-nesting-depth": 4,
+        "number-leading-zero": "always",
+        "indentation": "tab",
+        "function-parentheses-space-inside": "never",
+        "rule-empty-line-before": ["always", {
+            "except": ["first-nested"]
+        }], 
+        "color-hex-case": "upper",
+        "color-hex-length": "long",
+        "color-no-invalid-hex": true
+    }
+};
