fd473059e491f02ec0096d5feb5d3be3ffc7dfb9
lemnisSP <35028260+lemnisSP@users.noreply.github.com>
2018-03-30 12:26:27 +0200

    Small correction on our scss coding style

--- stylelint-config-studyportals/stylelint.config.js
+++ stylelint-config-studyportals/stylelint.config.js
@@ -6,14 +6,15 @@ module.exports = {
         "scss/at-mixin-pattern": "^[a-zA-Z]+$",
         "scss/percent-placeholder-pattern": "^[A-Z][a-zA-Z]*$",
         "scss/at-function-pattern": "^[a-zA-Z]*-?[a-zA-Z]*$",
-        "selector-class-pattern": "^[a-zA-Z]*$",
+        "selector-class-pattern": "^(is-)?[a-zA-Z]*$",
         "selector-id-pattern": "^[a-zA-Z]+$",
         "max-nesting-depth": 4,
         "number-leading-zero": "always",
         "indentation": "tab",
         "function-parentheses-space-inside": "never",
         "rule-empty-line-before": ["always", {
-            "except": ["first-nested"]
+            "except": ["first-nested"],
+            "ignore": ["after-comment", "first-nested"]
         }], 
         "color-hex-case": "upper",
         "color-hex-length": "long",
